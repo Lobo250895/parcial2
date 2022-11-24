4bd5ea83e0162148c42f07cd44b655c5b2d10998
Lobo250895 <estefania.lobo@davinci.edu.ar>
2022-11-24 16:46:14 -0300

    add getter and setter ans to string  in Square

--- Square.java
+++ Square.java
@@ -1,3 +1,29 @@
-public class Square {
+public class Square extends Rectangle {
+    private double side;
 
+    public Square(){
+        super();
+    }
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public Square(String color,boolean filled,double side){
+        super(color,filled);
+        this.side=side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{ side=" + side + "width" + "length"+ "}";
+    }
 }

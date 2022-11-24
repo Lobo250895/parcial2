c64e20d1958b01a6360c693b906857603cb3f4f2
Lobo250895 <estefania.lobo@davinci.edu.ar>
2022-11-24 15:02:09 -0300

    add class Circle

---
+++ Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+    private double radius = 1.0;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
+
